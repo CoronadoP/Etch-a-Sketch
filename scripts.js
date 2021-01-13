071fb7833fc46376a034b552114b59908ead940d
Piero Coronado <pierocoronado01@hotmail.com>
2021-01-12 20:28:19 -0500

    added css styling to website and fixed grid

--- scripts.js
+++ scripts.js
@@ -1,18 +1,34 @@
-let gridContainer = document.querySelector('#grid');
+//https://levelup.gitconnected.com/build-an-etch-a-sketch-knock-off-with-plain-js-css-and-html-9ab9e104b43f
+let grid = document.querySelector('#grid');
+let auto = "auto";
 
 function createGrid(){
+    
+    //Create necessary number of columns depending on # of squares per row/col
+    //Default 16x16 grid
+    let prop = "";
+    for(let i = 1; i < 17; i++){
+        prop += `${auto} `;
+    }
+    document.getElementById("grid").style['grid-template-columns'] = prop;
 
-    for(let i = 1; i <= 16; i++){
-        for(let j = 1; j <= 16; j++){
-            let gridSquare = document.createElement('div');
-            let squareId = `row-${i}col-${j}`;
-            gridSquare.classList.add("grid-square");
-            gridSquare.setAttribute('id', squareId);
-            gridContainer.appendChild(gridSquare);
-        }
+    //Create the necessary number of squares
+    for(let i = 0; i < 256; i++){
+        let gridSquare = document.createElement('div');
+        gridSquare.classList.add("grid-square");
+        grid.appendChild(gridSquare);
     }
     return;
 }
 
+function updateGrid(){
+
+}
 
 createGrid();
+
+
+
+
+
+
