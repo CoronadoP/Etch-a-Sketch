c75e9e382043ea7dec88b837847419d8f2b0c939
Piero Coronado <pierocoronado01@hotmail.com>
2021-01-13 19:56:24 -0500

    working on changing color function

--- scripts.js
+++ scripts.js
@@ -13,19 +13,38 @@ function createGrid(){
     document.getElementById("grid").style['grid-template-columns'] = prop;
 
     //Create the necessary number of squares
+    /*
     for(let i = 0; i < 256; i++){
         let gridSquare = document.createElement('div');
         gridSquare.classList.add("grid-square");
         grid.appendChild(gridSquare);
     }
+    */
+    for(let r = 1; r < 17; r++){
+        for(let c = 1; c < 17; c++){
+            let gridSquare = document.createElement('div');
+            gridSquare.classList.add("grid-square");
+            gridSquare.setAttribute('id', `r${r}c${c}`);
+            grid.appendChild(gridSquare);
+        }
+    }
+
     return;
 }
 
-function updateGrid(){
 
-}
 
 createGrid();
+
+//Use this to get started on changing color of grid square
+document.getElementById("r1c1").onmouseover = function() {
+    mouseOver()
+};
+
+function mouseOver(){
+    document.getElementById("r1c1").style['background-color'] = "black";
+}
+
 
 
 
